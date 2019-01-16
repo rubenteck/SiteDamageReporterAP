f082e370c6caa80e22c57c0ee74da83a96ee0919
daank <32707088+daankennes@users.noreply.github.com>
2019-01-16 16:30:18 +0100

    Klaargemaakt voor Firebase hosting
    https://apdamagereporter.firebaseapp.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCPwenlsC4gx6Dkb8I9ZSH027W21oylyjw",
+    authDomain: "apdamagereporter.firebaseapp.com",
+    databaseURL: "https://apdamagereporter.firebaseio.com",
+    projectId: "apdamagereporter",
+    storageBucket: "apdamagereporter.appspot.com",
+    messagingSenderId: "83282732211"
+  }
 };
